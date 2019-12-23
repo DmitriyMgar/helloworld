b6e104eef888b04dd4edd43f183eb662486fe8c4
Dmitriy Mgar <mgar@phystech.edu>
2019-12-23 05:56:10 +0300

    Python. Модули и библиотеки. Задание 1.3

--- module2.py
+++ module2.py
@@ -5,7 +5,9 @@ def get_random_choice(some_list):
     if not some_list:
         return None
     else:
-        return random.choice(some_list)
+        my_choice = random.choice(some_list)
+        print("Я выбрал {}".format(my_choice))
+        return my_choice
 
 
 print(get_random_choice([1, 2, 3, 4]))
